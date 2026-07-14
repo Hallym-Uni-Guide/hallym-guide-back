32cc38440d70c9f03cc4cc8315b96d04c8a4caa7
Eula_association <hoperoy1213@gmail.com>
2026-07-14 10:59:33 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+//임시
+const users = [
+    {
+        id: "user1",
+        password: "1234",
+        name: "홍길동",
+        major: "소프트웨어학부",
+        interest: "백엔드 개발",
+        intro: "안녕하세요."
+    }
+];
+
+module.exports = users;
